a9a06981d9bca8b966f01cd66a0a9fbbc0d99f74
PratikC54 <pratiksenapati54@gmail.com>
2026-07-22 22:09:14 +0530

    upgrading applications with Gemini services

---
+++ aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -0,0 +1,42 @@
+package com.fitness.aiservice.service;
+
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@NoArgsConstructor
+@Service
+public class GeminiService {
+
+    private WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public GeminiService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+    public String getRecommendations(String details) {
+        Map<String, Object> requestBody = Map.of("contains",new Object[] {
+                Map.of("parts", new Object[] {
+                        Map.of("text", details)
+                })
+        });
+        String response = webClient.post()
+                .uri(geminiApiUrl)
+                .header("content-Type", "applciation/json")
+                .header("X-goog-api-key", geminiApiKey)
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        return response ;
+    }
+}
